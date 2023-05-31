112f7c5e3b4c1b7f16af1158703e0aaf59f33020
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-05-30 21:26:05 -0500

    Update Nextjs Formik and Tailwind

---
+++ src/app/list.tsx
@@ -0,0 +1,330 @@
+interface ListCountry {
+    id: number,
+    title: string
+}
+
+const Country: ListCountry[] = [
+    {
+      id: 1,
+      title: "Afganistán",
+     
+    },
+    {
+      id: 2,
+      title: "Albania",
+
+    },
+    {
+      id: 3,
+      title: "Alemania",
+    
+    },
+    {
+      id: 4,
+      title: "Andorra",
+     
+    },
+    {
+        id: 5,
+        title: "Arabia Saudita",
+       
+      },
+      {
+        id: 6,
+        title: "Angola",
+       
+      },{
+        id: 7,
+        title: "Argelia"
+      },
+      {
+        id: 8,
+        title: "Antigua y Barbuda"
+      },
+      {
+        id: 9,
+        title: "Argentina"
+      },
+      {
+        id: 10,
+        title: "Austria"
+      },
+      {
+        id: 11,
+        title: "Bahamas"
+      },
+      {
+        id: 12,
+        title: "Bélgica"
+      },
+      {
+        id: 13,
+        title: "Bolivia"
+      },
+      {
+        id: 14,
+        title: "Brasil"
+      },
+      {
+        id: 15,
+        title: "Bulgaria"
+      },
+      {
+        id: 16,
+        title: "Camerún"
+      },
+      {
+        id: 17,
+        title: "Canadá"
+      },
+      {
+        id: 18,
+        title: "Catar"
+      },
+      {
+        id: 19,
+        title: "Chile"
+      },
+      {
+        id: 20,
+        title: "China"
+      },
+      {
+        id: 21,
+        title: "Ciudad del Vaticano"
+      },
+      {
+        id: 22,
+        title: "Colombia"
+      },
+      {
+        id: 23,
+        title: "Corea del Norte"
+      },
+      {
+        id: 24,
+        title: "Corea del Sur"
+      },
+      {
+        id: 25,
+        title: "Costa Rica"
+      },
+      {
+        id: 24,
+        title: "Cuba"
+      },
+      {
+        id: 27,
+        title: "Dinamarca"
+      },
+      {
+        id: 28,
+        title: "Ecuador"
+      },
+      {
+        id: 29,
+        title: "Egipto"
+      },
+      {
+        id: 30,
+        title: "El Salvador"
+      },
+      {
+        id: 31,
+        title: "Emiratos Árabes Unidos"
+      },
+      {
+        id: 32,
+        title: "España"
+      },
+      {
+        id: 33,
+        title: "Estados Unidos"
+      },
+      {
+        id: 34,
+        title: "Filipinas"
+      },
+      {
+        id: 35,
+        title: "Finlandia"
+      },
+      {
+        id: 36,
+        title: "Francia"
+      },
+      {
+        id: 37,
+        title: "Guatemala"
+      },
+      {
+        id: 38,
+        title: "Grecia"
+      },
+      {
+        id: 39,
+        title: "Haití"
+      },
+      {
+        id: 40,
+        title: "Honduras"
+      },
+      {
+        id: 41,
+        title: "India"
+      },
+      {
+        id: 42,
+        title: "Israel"
+      },
+      {
+        id: 43,
+        title: "Italia"
+      },
+      {
+        id: 44,
+        title: "Jamaica"
+      },
+      {
+        id: 45,
+        title: "Japon"
+      },
+      {
+        id: 46,
+        title: "Malí"
+      },
+      {
+        id: 47,
+        title: "Malta"
+      },
+      {
+        id: 47,
+        title: "México"
+      },
+      {
+        id: 48,
+        title: "Madagascar"
+      },
+      {
+        id: 49,
+        title: "Nigeria"
+      },
+      {
+        id: 50,
+        title: "Noruega"
+      },
+      {
+        id: 51,
+        title: "Paraguay"
+      },
+      {
+        id: 52,
+        title: "Perú"
+      },
+      {
+        id: 53,
+        title: "Polonia"
+      },
+      {
+        id: 54,
+        title: "Portugal"
+      },
+      {
+        id: 55,
+        title: "Reino Unido"
+      },
+      {
+        id: 56,
+        title: "República Checa"
+      },
+      {
+        id: 57,
+        title: "República Dominicana"
+      },
+      {
+        id: 58,
+        title: "Rusia"
+      },
+      {
+        id: 59,
+        title: "Senegal"
+      },
+      {
+        id: 60,
+        title: "Serbia"
+      },
+      {
+        id: 61,
+        title: "Singapur"
+      },
+      {
+        id: 62,
+        title: "Siria"
+      },
+      {
+        id: 63,
+        title: "Suecia"
+      },
+      {
+        id: 64,
+        title: "Suiza"
+      },
+      {
+        id: 65,
+        title: "Tailandia"
+      },
+      {
+        id: 66,
+        title: "Togo"
+      },
+      {
+        id: 67,
+        title: "Tonga"
+      },
+      {
+        id: 68,
+        title: "Turquia"
+      },
+      {
+        id: 69,
+        title: "Túnez"
+      },
+      {
+        id: 70,
+        title: "Ucrania"
+      },
+      {
+        id: 71,
+        title: "Uruguay"
+      },
+      {
+        id: 72,
+        title: "Venezuela"
+      },
+      {
+        id: 74,
+        title: "Zambia"
+      },{
+        id: 75,
+        title: "Zimbabue"
+      }
+ ] 
+ export default Country;
+
+ 
+	
+	
+	
+	
+	
+	
+	
+	
+		
+
+		
+	
+	
+	
+	
+		
